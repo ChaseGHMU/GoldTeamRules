848b86e8098011fb041bf194fd560826b8d02839
Chase Allen <challenj23@gmail.com>
2017-11-28 01:28:52 -0600

    CSS and some view

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var express = require('express');
 var socket = require('socket.io');
 
 var app = express();
-// var http = require('http').Server(app);
 var server = app.listen(4000);
 var io = socket(server);
 
