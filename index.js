31477ac2c64f205d4cb223d15b9245c3b48ccacd
Chase Allen <challenj23@gmail.com>
2017-11-28 00:31:51 -0600

    broadcasting changes

--- index.js
+++ index.js
@@ -13,17 +13,7 @@ app.get('/', function(req,res){
 io.on('connection', function(socket){
   io.emit('connected');
 
-  socket.on('sendNickname', function(name){
-    socket.nickname = name
-    io.emit('user-connected', socket.nickname);
-  });
-
-  socket.on('disconnect',function(){
-    io.emit('user-disconnect', socket.nickname);
-  });
-
-  socket.on('chat message', function(msg){
-    msg = socket.nickname + ": " + msg;
-    io.emit('chat message', msg);
-  });
+  socket.on('clicked', function(){
+    socket.broadcast.emit('clicked');
+  })
 });
